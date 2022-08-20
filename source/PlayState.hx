c22050c3c40cb5d11297500db657d49600817e01
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-08-19 22:48:10 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2625,7 +2625,7 @@ var repositionShit = 350;
 	}
 
 	public function startVideo(name:String):Void {
-		#if VIDEOS_ALLOWED
+		#if desktop
 		var foundFile:Bool = false;
 		var fileName:String = #if MODS_ALLOWED Paths.modFolders('videos/' + name + '.' + Paths.VIDEO_EXT); #else ''; #end
 		#if sys
@@ -2645,7 +2645,31 @@ var repositionShit = 350;
 			}
 		}
 
-		
+		if(foundFile) {
+			inCutscene = true;
+			var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
+			bg.scrollFactor.set();
+			bg.cameras = [camHUD];
+			add(bg);
+
+
+			(new FlxVideo(fileName)).finishCallback = function() {
+				remove(bg);
+				startAndEnd();
+			}
+
+
+			return;
+		}
+		else
+		{
+			FlxG.log.warn('Couldnt find video file: ' + fileName);
+			startAndEnd();
+		}
+		#end
+		startAndEnd();
+	}
+
 	
 	function startAndEnd()
 	{
